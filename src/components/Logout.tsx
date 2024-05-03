1c5d93074a12a681bb8c37a1da2b225395e1966a
tomurayuri <61407899+tomurayuri@users.noreply.github.com>
2024-05-03 14:27:06 +0900

    Initial commit

---
+++ src/components/Logout.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Logout: React.FC = () => {
+  return <div>Logout★★</div>;
+};
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCy8mFDBsfnlV9QIzqTlB7MVu9njJ5GnX0',
+  authDomain: 'blog-from-2024.firebaseapp.com',
+  projectId: 'blog-from-2024',
+  storageBucket: 'blog-from-2024.appspot.com',
+  messagingSenderId: '303993818865',
+  appId: '1:303993818865:web:22f889bc90a8dda178a350',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const ptovider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export { auth, ptovider, db };
