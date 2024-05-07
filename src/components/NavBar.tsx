60354a5981b2c81e7b452449aa20cec9a2d3b5eb
tomurayuri <yuri.tomura@nijibox.co.jp>
2024-05-07 23:52:26 +0900

    FontAwesomeIconの使用

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouseChimney, faFilePen ,faRightToBracket} from '@fortawesome/free-solid-svg-icons';
 
 export const NavBar: React.FC = () => {
   return (
     <nav>
-        <Link to="/">ホーム</Link>
-        <Link to="/CreatePost">記事投稿</Link>
-        <Link to="/Login">ログイン</Link>
+        <Link to="/">
+            <FontAwesomeIcon icon={faHouseChimney} />ホーム
+        </Link>
+        <Link to="/CreatePost">
+            <FontAwesomeIcon icon={faFilePen} />記事投稿
+        </Link>
+        <Link to="/Login">
+            <FontAwesomeIcon icon={faRightToBracket} />ログイン
+        </Link>
     </nav>
     )
 };
