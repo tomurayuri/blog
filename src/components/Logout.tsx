bb09b7c8dc055a7100b393a59d974ee1e1b5fcbc
tomurayuri <yuri.tomura@nijibox.co.jp>
2024-05-12 20:36:29 +0900

    Googleログアウト機能

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import { signOut } from 'firebase/auth';
+import { auth} from '../firebase';
+import { useNavigate } from 'react-router-dom';
+type Props = {
+  setIsAuth: (isLogin:boolean) => void
+}
 
-export const Logout: React.FC = () => {
-  return <div>Logout★★</div>;
+export const Logout = (props:Props) => {
+  const { setIsAuth } = props;
+  const navigate = useNavigate();
+  const logout = () => {
+    // ログアウト
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      navigate("/login")
+
+    }).catch((error)=> {
+      console.log(error)
+    })
+
+  }
+  return (
+  <div>
+    <p>ログアウトする</p>
+    <button onClick={logout}>ログアウト</button>
+  </div>
+  )
 };
